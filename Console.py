be82bac953155e7c6b0685930922af6b648ff6a3
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2024-01-02 15:45:59 +0900

    状態の定義、仮のタイトルロゴ処理

--- Console.py
+++ Console.py
@@ -1,6 +1,9 @@
 import os
 
-def transferScreen(screenBuf):
+def Init():
+    pass
+
+def TransferScreen(screenBuf):
     os.system("clear")
     for r in range(2):
         for c in range(10):
--- Invador.py
+++ Invador.py
@@ -1,6 +1,7 @@
 import Console as Dev
 import time
 import threading
+from enum import Enum
 
 screenBuffer = [ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "),
     ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord("0")]
@@ -14,20 +15,53 @@ def Render():
         tm.cancel()
         del tm
         #転送
-        Dev.transferScreen(screenBuffer)
+        Dev.TransferScreen(screenBuffer)
         #フレーム共通処理を動かす
         isRenderOk = False
         #描画スレッド待機
         tm = threading.Timer(1, Render)
         tm.start()
 
+#ASCII文字の書き込み
+def SetAscii(row, column, code):
+    screenBuffer[row * 10 + column] = code
+
+#ASCII文字列の書き込み
+def SetAsciiStr(row, column, str):
+    strlen = 0;
+    code = 0
+    for i in range(strlen):
+        SetAscii(row, column + i, code)
+
+#初期化
+Dev.Init()
+
 #描画スレッドスタート
 tm = threading.Timer(1, Render)
 tm.start()
 isRenderOk = False
 
+#状態定義
+class State(Enum):
+    TITLE_LOGOANIMATION = 1,
+    TITLE_WAIT = 2,
+    GAME_READY = 3,
+    GAME_MAIN = 4,
+    GAME_GAMEOVER = 5,
+
+#タイトル
+def TitleLogo():
+    screenBuffer[19] += 1
+
+#
+
+#状態関数
+state = {State.TITLE_LOGOANIMATION: TitleLogo}
+
+currentState = State.TITLE_LOGOANIMATION
+
 #フレーム共通処理
 while True:
     if not isRenderOk:
-        screenBuffer[19] += 1
+        state[currentState]()
         isRenderOk = True
