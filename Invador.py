ba809ebf340f8e3a118a5f5dc3020aa4477beba1
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2024-01-02 12:09:56 +0900

    メインループ処理を作成

--- Invador.py
+++ Invador.py
@@ -1,6 +1,33 @@
-import Console
+import Console as Dev
+import time
+import threading
 
 screenBuffer = [ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "),
     ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord(" "), ord("0")]
 
-Console.transferScreen(screenBuffer)
+#描画共通処理
+def Render():
+    global isRenderOk
+    if isRenderOk:
+        #コールバック一時解除
+        global tm
+        tm.cancel()
+        del tm
+        #転送
+        Dev.transferScreen(screenBuffer)
+        #フレーム共通処理を動かす
+        isRenderOk = False
+        #描画スレッド待機
+        tm = threading.Timer(1, Render)
+        tm.start()
+
+#描画スレッドスタート
+tm = threading.Timer(1, Render)
+tm.start()
+isRenderOk = False
+
+#フレーム共通処理
+while True:
+    if not isRenderOk:
+        screenBuffer[19] += 1
+        isRenderOk = True
