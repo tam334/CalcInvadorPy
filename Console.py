23126b4a7c52655e589611ef425993c55bbadbdf
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2024-01-03 14:32:26 +0900

    キーボードがうまく動かないので一旦コメントアウト かつ、初回以外の画面クリアはカーソルを戻すことで代用する

--- Console.py
+++ Console.py
@@ -1,29 +1,29 @@
 import os
-from pynput import keyboard
 
 def OnPressKey(key):
-    global buttonBuffer
-    if key.char == 'z':
-        buttonBuffer |= 0x01
-    elif key.char == 'x':
-        buttonBuffer |= 0x02
+    #global buttonBuffer
+    #if key.char == 'z':
+    #    buttonBuffer |= 0x01
+    #elif key.char == 'x':
+    #    buttonBuffer |= 0x02
     return True
 
 def Init():
     global buttonBuffer
     buttonBuffer = 0
-    listener = keyboard.Listener(on_press=OnPressKey)
-    listener.start()
+    #listener = keyboard.Listener(on_press=OnPressKey)
+    #listener.start()
+    os.system("clear")
 
 def TransferScreen(screenBuf):
-    os.system("clear")
+    print("\033[2A", end="")
     for r in range(2):
-        for c in range(20):
-            print(chr(screenBuf[r * 20 + c]), end="")
+        for c in range(16):
+            print(chr(screenBuf[r * 16 + c]), end="")
         print("", end="\n")
 
 def PopCurrentButton():
     global buttonBuffer
-    button = buttonBuffer
+    button = 0x2
     buttonBuffer = 0
     return button
